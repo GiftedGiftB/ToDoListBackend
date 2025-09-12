c06e2b011f31f70b35da6220f15e9ef5d6632006
GiftedGiftB <asekhamegift@gmail.com>
2025-09-12 02:28:19 +0100

    user class

--- src/main/java/com/TodoList/data/models/User.java
+++ src/main/java/com/TodoList/data/models/User.java
@@ -1,32 +1,20 @@
 package com.TodoList.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    @id
+    @Id
     private String id;
-    private String userName;
+    private String username;
+    private boolean email;
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
