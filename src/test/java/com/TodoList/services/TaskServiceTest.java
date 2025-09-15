89bd05f853d900f5edf6ed8defa04220f2cb3dd5
GiftedGiftB <asekhamegift@gmail.com>
2025-09-15 23:25:19 +0100

    still working on the Todo App

--- src/test/java/com/TodoList/services/TaskServiceTest.java
+++ src/test/java/com/TodoList/services/TaskServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.*;
 
 @SpringBootTest
 public class TaskServiceTest {
@@ -29,20 +28,23 @@ public class TaskServiceTest {
         Task task = new Task();
         task.setTitle("Buy groceries");
         task.setDescription("milk, bread, eggs");
-        Task savedTask = taskService.save(task);
+        Task savedTask = taskService.createTask(task);
         Assertions.assertNotNull(savedTask.getId());
         Assertions.assertEquals("Buy groceries", savedTask.getTitle());
         Assertions.assertTrue(taskRepository.existsById(savedTask.getId()));
     }
 
     @Test
-    public void markTaskAsCompleted(){
+    public void markTaskAsCompleted() {
         Task task = new Task();
         task.setTitle("Read book");
         task.setDescription("clean code");
-        Task savedTask = taskService.save(task);
+
+        Task savedTask = taskService.createTask(task);
+
         Task completedTask = taskService.markAsCompleted(savedTask.getId());
         Assertions.assertTrue(completedTask.isCompleted());
     }
+
 
 }
