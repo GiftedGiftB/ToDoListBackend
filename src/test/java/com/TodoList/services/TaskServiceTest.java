b4a6f17197473f832727e6ec97e06c03b234b130
GiftedGiftB <asekhamegift@gmail.com>
2025-09-17 23:17:54 +0100

    ToDo app

--- src/test/java/com/TodoList/services/TaskServiceTest.java
+++ src/test/java/com/TodoList/services/TaskServiceTest.java
@@ -4,6 +4,8 @@ import com.TodoList.data.models.Task;
 import com.TodoList.data.models.User;
 import com.TodoList.data.repositories.TaskRepository;
 import com.TodoList.data.repositories.UserRepository;
+import com.TodoList.dtos.requests.TaskRequest;
+import com.TodoList.dtos.responses.TaskResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TaskServiceTest {
+
+    TaskRequest request;
     @Autowired
     private UserRepository userRepository;
 
@@ -23,18 +27,21 @@ class TaskServiceTest {
     private TaskRepository taskRepository;
 
     @BeforeEach
-    void cleanDatabase() {
-        taskRepository.deleteAll();
+    void setUp() {
+        request = new TaskRequest();
+        request.setTitle("gift");
+        request.setDescription("gift");
+        TaskResponse response = new TaskResponse();
     }
 
     @Test
     void addTask() {
         Task task = new Task();
-        task.setTitle("Buy groceries");
-        task.setDescription("milk, bread, eggs");
+        task.setTitle("Community hangout");
+        task.setDescription("Hangout");
         Task savedTask = taskService.addTask(task);
         assertNotNull(savedTask.getId());
-        assertEquals("Buy groceries", savedTask.getTitle());
+        assertEquals("Community hangout", savedTask.getTitle());
         assertTrue(taskRepository.existsById(savedTask.getId()));
     }
 
@@ -45,12 +52,14 @@ class TaskServiceTest {
         user.setEmail("gift@gmail.com");
         User savedUser = userRepository.save(user);
         Task task = new Task();
-        task.setTitle("Read book");
-        task.setDescription("clean code");
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
         task.setUserId(user.getId());
         Task savedTask = taskService.addTask(task);
+        assertFalse(savedTask.isCompleted());
         Task completedTask = taskService.markCompleted(savedTask.getId(), savedUser.getId());
         assertTrue(completedTask.isCompleted());
     }
+    
 }
 
