c4f7640f0c02ada6457995588b2bdf1a2c655a24
GiftedGiftB <asekhamegift@gmail.com>
2025-09-19 23:06:47 +0100

    project still in progress

--- src/main/java/com/TodoList/dtos/responses/UserResponse.java
+++ src/main/java/com/TodoList/dtos/responses/UserResponse.java
@@ -1,10 +1,12 @@
 package com.TodoList.dtos.responses;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserResponse {
     private String id;
+    private String username;
     private String email;
 }
