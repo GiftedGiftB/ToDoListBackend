0fa73553c33c3feb97aca60289e6b5cb4067e804
GiftedGiftB <asekhamegift@gmail.com>
2025-09-19 22:08:36 +0100

    fixed TaskResponse

--- src/main/java/com/TodoList/dtos/responses/TaskResponse.java
+++ src/main/java/com/TodoList/dtos/responses/TaskResponse.java
@@ -1,12 +1,15 @@
 package com.TodoList.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskResponse {
     private String id;
     private String title;
     private String description;
     private boolean completed;
-
 }
--- src/main/java/com/TodoList/services/UserService.java
+++ src/main/java/com/TodoList/services/UserService.java
@@ -1,29 +1,4 @@
 package com.TodoList.services;
 
-import com.TodoList.data.models.User;
-import com.TodoList.data.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    public User register(User user) {
-        if (user.getEmail() == null || user.getPassword() == null || user.getUsername() == null) {
-            throw new IllegalArgumentException("All fields are required");
-        }
-        return userRepository.save(user);
-    }
-
-    public User login(String email, String password) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
-
-        if (!user.getPassword().equals(password)) {
-            throw new IllegalArgumentException("Invalid password");
-        }
-        return user;
-    }
+public interface UserService {
 }
