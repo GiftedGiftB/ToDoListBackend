b66c71a2e342124eab7b3630791dc177e1d2a61b
GiftedGiftB <asekhamegift@gmail.com>
2025-09-20 20:40:30 +0100

    made some changes

--- src/test/java/com/TodoList/services/UserServiceTest.java
+++ src/test/java/com/TodoList/services/UserServiceTest.java
@@ -1,13 +1,14 @@
 package com.TodoList.services;
 
-import com.TodoList.data.models.User;
 import com.TodoList.data.repositories.UserRepository;
+import com.TodoList.dtos.requests.UserRequest;
+import com.TodoList.dtos.responses.UserResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -26,14 +27,35 @@ public class UserServiceTest {
 
     @Test
     public void testRegisterNewUser(){
-        User user = new User();
-        user.setPassword("12345");
-        user.setEmail("gift@gmail.com");
-
-        User savedUser = userRepository.save(user);
+        UserRequest request = new UserRequest();
+        request.setUsername("Gift");
+        request.setEmail("gift@gmail.com");
+        request.setPassword("12345");
+        UserResponse savedUser = userService.register(request);
         assertEquals("gift@gmail.com", savedUser.getEmail());
         assertTrue(userRepository.existsByEmail("gift@gmail.com"));
     }
 
+    @Test
+    public void testLoginUser(){
+        UserRequest request = new UserRequest();
+        request.setUsername("Gift");
+        request.setEmail("gift@gmail.com");
+        request.setPassword("12345");
+        userService.register(request);
+        UserResponse response = userService.login("gift@gmail.com", "12345");
+        assertEquals("gift@gmail.com", response.getEmail());
+    }
 
+    @Test
+    public void testLoginInvalidPassword() {
+        UserRequest request = new UserRequest();
+        request.setUsername("Gift");
+        request.setEmail("gift@gmail.com");
+        request.setPassword("12345");
+        userService.register(request);
+        Exception ex = assertThrows(RuntimeException.class, () -> userService.login("gift@gmail.com", "wrongpass"));
+        assertTrue(ex.getMessage().contains("Invalid password"));
+    }
+    
 }
