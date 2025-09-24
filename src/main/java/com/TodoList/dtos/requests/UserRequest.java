71bf667342d4ac212a3702a481a910599405ba7a
GiftedGiftB <asekhamegift@gmail.com>
2025-09-24 23:49:00 +0100

    user request class

--- src/main/java/com/TodoList/dtos/requests/UserRequest.java
+++ src/main/java/com/TodoList/dtos/requests/UserRequest.java
@@ -1,8 +1,12 @@
 package com.TodoList.dtos.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
     private String username;
     private String email;
